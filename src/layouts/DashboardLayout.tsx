83e2dbb87ad31e3007bf35d01538e6175f864762
LuisRha <127280301+LuisRha@users.noreply.github.com>
2026-08-24 22:50:03 -0500

    style: slightly darker blue bg

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { DashboardSidebar } from '@/components/layout/DashboardSidebar'
 
 export function DashboardLayout() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-sky-50 via-blue-50 to-indigo-50">
+    <div className="min-h-screen bg-gradient-to-br from-sky-100 via-blue-100 to-indigo-100">
       <DashboardNavbar />
       <DashboardSidebar />
       <main className="lg:pl-64 pt-0">
